e51df89d9b57cd671aa97f015372d1ad1d96db69
krisztin <k.nagy@wellcome.ac.uk>
2018-05-14 10:43:58 +0100

    adding flex gallery js exercise

---
+++ js30/05flexGallery/flex.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+function open() {
+    this.classList.toggle('open');
+}
+
+function active(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', open));
+panels.forEach(panel => panel.addEventListener('transitionend', active));
