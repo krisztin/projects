de8c5de63e85bf42b2309fec7660683cbb9ce541
krisztin <k.nagy@wellcome.ac.uk>
2018-05-14 11:56:22 +0100

    fixing the 90deg issue

--- js30/02clock/clock.js
+++ js30/02clock/clock.js
@@ -7,15 +7,15 @@ function setDate() {
   const now = new Date();
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + 90;
+  const hoursDegrees = ((hours / 12) * 360);
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
   const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const minutesDegrees = ((minutes / 60) * 360);
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   if (secondsDegrees == 0) {
