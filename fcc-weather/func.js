c9e827b05a0fa42e01bd10e428f9a8d608de9d96
krisztin <k.nagy@wellcome.ac.uk>
2018-05-30 19:47:09 +0100

    done?

--- fcc-weather/func.js
+++ fcc-weather/func.js
@@ -1,21 +1,18 @@
 // HTML elements
-const wrapper = document.getElementById("wrapper");
-const tempValue = document.getElementById("temp");
-const tempScale = document.getElementById("cf");
-const icon = document.getElementById("icon");
 const fahBtn = document.getElementById("fahBtn");
 const celsBtn = document.getElementById("celsBtn");
+let output = document.getElementById("wrapper");
 
-// Button clicky clicky
+// Button clicky clicky aka EVENTS
 fahBtn.addEventListener("click", convertToF);
-celsBtn.addEventListener("click", getTemp);
+celsBtn.addEventListener("click", convertToC);
 
 // Data
 let localTemp = 0;
 
 function getTemp() {
   if (!navigator.geolocation) {
-    wrapper.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
   }
   function success(position) {
     //#1 grabbing coordinates
@@ -25,29 +22,36 @@ function getTemp() {
     $.getJSON(
       "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon
     ).done(data => {
-      let localTemp = Math.round(data.main.temp);
-      //#3a adding cels and symbol
-      temp.innerHTML = localTemp;
-      tempScale.innerHTML = "°C";
-      //#3b icons and descriptions of current weather conditions
-      icon.innerHTML =
-        "<img src = '" +
-        data.weather[0].icon +
-        "'>" +
-        data.weather[0].description;
+      localTemp = Math.round(data.main.temp);
+      output.innerHTML =
+        `<p>It is <span id="temp">${localTemp}</span>
+        <span id="cf">°C</span>
+        in <span>${data.name}</span>.
+        If you look outside, you should see <span>${data.weather[0].description}</span>.</p>`
     });
   }
   function error() {
-    wrapper.innerHTML = "<p>Couldn't retrieve your location</p>";
+    output.innerHTML = "<p>Couldn't retrieve your location</p>";
   }
+  output.innerHTML = "<p>Loading...</p>"
   navigator.geolocation.getCurrentPosition(success, error);
 }
 
 // C to F converter
 function convertToF() {
-  const fahrenheit = Math.round(localTemp * 9 / 5 + 32);
-  tempValue.innerHTML = fahrenheit;
+  const tempScale = document.getElementById("cf");
+  const tempValue = document.getElementById("temp");
+  
+  tempValue.innerHTML = Math.round(localTemp * 9 / 5 + 32);
   tempScale.innerHTML = "°F";
+}
+
+function convertToC() {
+  const tempScale = document.getElementById("cf");
+  const tempValue = document.getElementById("temp");
+  
+  tempValue.innerHTML = localTemp;
+  tempScale.innerHTML = "°C";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
