60ef2a1507c9079cdf31d6c6c6918d8a14bde1c9
krisztin <k.nagy@wellcome.ac.uk>
2018-06-28 09:54:48 +0100

    js30 link highlighting that follows you on page

---
+++ js30/22followLinks/highlight.js
@@ -0,0 +1,24 @@
+const triggers = document.querySelectorAll('a');
+
+//create highlight element (the little pill that will follow the mouse and resize)
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect(); //gBC gets coordinate info of each element
+
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,  // when scrolling down or sideways the highlight goes offset without this
+    left: linkCoords.left + window.scrollX,
+  }
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
