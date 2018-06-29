74281a6e1d750efabb1bcd873d10bf42391cb30f
krisztin <k.nagy@wellcome.ac.uk>
2018-06-29 22:28:52 +0100

    js30 copying stripes cool follow along nav background

---
+++ js30/26stripeFollowAlongNav/follow.js
@@ -0,0 +1,41 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter() {
+  this.classList.add('trigger-enter');
+  setTimeout(() => {
+    // to solve content fadein lag during quick mouse movement
+    if (this.classList.contains('trigger-enter')) {
+      this.classList.add('trigger-enter-active');
+    }
+  }, 150);
+
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+
+  // where the dropdowns at?
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  // coordinates to help calc for nav being offset (and it will be unless it's on the top of doc)
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left,
+  };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+}
+
+function handleLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
