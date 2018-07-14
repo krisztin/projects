7e53d5a9df2dbf6bc46b9bfd8e995115119b14a3
krisztin <k.nagy@wellcome.ac.uk>
2018-07-14 19:47:23 +0100

    delete function

--- issue-tracker/app.js
+++ issue-tracker/app.js
@@ -69,3 +69,17 @@ function setStatusClosed(id) {
 
   fetchIssues();
 }
+
+function deleteIssue(id) {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id === id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
